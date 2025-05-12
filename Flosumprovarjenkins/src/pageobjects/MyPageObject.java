fe8745c0e76299d854dd99424fcdf2de11bf8e47
srirangakulkarni-cp <murali.shika@cloudperitus.com>
2025-05-12 17:06:19 +0530

    test case new

--- Flosumprovarjenkins/src/pageobjects/MyPageObject.java
+++ Flosumprovarjenkins/src/pageobjects/MyPageObject.java
@@ -20,5 +20,22 @@ public class MyPageObject {
 	@LinkType()
 	@FindBy(xpath = "//a[normalize-space(.)='Gmail']")
 	public WebElement gmail;
-			
+	@LinkType()
+    @FindBy(xpath = "//a[normalize-space(.)='Accounts']")
+    public WebElement AccountsTab;
+    @LinkType()
+    @FindBy(xpath = "//a[normalize-space(.)='New']")
+    public WebElement NewButton;
+    @TextType()
+    @FindBy(xpath="//label[text()='Industry']/following::div[contains(@class,'slds-form-element__help')][1]") 
+    public WebElement IndustryErrorMessage;
+    @TextType()
+    @FindBy(xpath = "//div[contains(@class, 'active') and contains(@class, 'open')]//input[@name='Name']")
+    public WebElement AccountName;
+    @TextType()
+    @FindBy(xpath = "//div[contains(@class, 'active') and contains(@class, 'open')]//button[@aria-label='Industry']")
+    public WebElement Industry;
+    @ButtonType()
+    @FindBy(xpath = "//div[contains(@class, 'active') and contains(@class, 'open')]//button[normalize-space(.)='Save']")
+    public WebElement SaveButton;			
 }
